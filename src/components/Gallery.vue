9202aa40199ad971a0b7f25f40b7675fc82e0174
Kristian Muñoz <kristian.munoz@codurance.com>
2020-04-07 16:07:22 +0200

    Add a renderer for each item of the results array

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,14 +1,21 @@
 <template>
-    <div v-if="results.length == 0">No items found</div>
+	<div class="gallery">
+		<div v-if="results.length == 0">No items found</div>
+		<section v-else>
+            <div v-for="(result, key) in results" :key="key">
+                {{ result }}
+            </div>
+        </section>
+	</div>
 </template>
 <script>
 export default {
-    name: "Gallery",
-    props: {
-        results: {
-            type: Array,
-            default: []
-        } 
-    } 
-}
+	name: "Gallery",
+	props: {
+		results: {
+			type: Array,
+			required: true
+		}
+	}
+};
 </script>
--- tests/unit/Gallery.spec.js
+++ tests/unit/Gallery.spec.js
@@ -2,7 +2,27 @@ import { shallowMount } from '@vue/test-utils'
 import Gallery from '@/components/Gallery'
 
 test('Should print empty message when no results found.', () => {
-    const wrapper = shallowMount(Gallery)
+    const component = shallowMount(Gallery, {
+        propsData: {
+            results: []
+        }
+    })
 
-    expect(wrapper.text()).toContain("No items found")
+    expect(component.text()).toContain("No items found")
+})
+
+test('Should print each result item.', () => {
+    const firstItem = "http://www.this-is-a-link.com"
+    const secondItem = "http://www.this-is-aother-link.com"
+    const component = shallowMount(Gallery, {
+        propsData: {
+            results: [
+                firstItem,
+                secondItem
+            ]
+        }
+    })
+
+    expect(component.text()).toContain(firstItem)
+    expect(component.text()).toContain(secondItem)
 })
