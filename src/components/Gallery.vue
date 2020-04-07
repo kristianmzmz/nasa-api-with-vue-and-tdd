6dc525cb6937e9dc2d3dca18f8d782ef0deeb0f0
Kristian Muñoz <kristian.munoz@codurance.com>
2020-04-07 16:10:31 +0200

    Add some styles for the gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="gallery">
+	<div class="gallery cf">
 		<div v-if="results.length == 0">No items found</div>
 		<section v-else>
             <div v-for="(result, key) in results" :key="key">
@@ -19,3 +19,47 @@ export default {
 	}
 };
 </script>
+
+<style scoped scss>
+.gallery {
+	width: 640px;
+	margin: 0 auto;
+	padding: 5px;
+	background: #fff;
+	box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+}
+
+.gallery > div {
+	position: relative;
+	float: left;
+	padding: 5px;
+	min-height: 220px;
+}
+
+.gallery > div > img {
+	width: 200px;
+	vertical-align: middle;
+	transition: 0.1s transform;
+	transform: translateZ(0); /* hack */
+}
+
+.gallery > div:hover {
+	z-index: 1;
+}
+
+.gallery > div:hover > img {
+	transform: scale(1.8, 1.8);
+	transition: 0.3s transform;
+}
+
+.cf:before,
+.cf:after {
+	display: table;
+	content: "";
+	line-height: 0;
+}
+
+.cf:after {
+	clear: both;
+}
+</style>
